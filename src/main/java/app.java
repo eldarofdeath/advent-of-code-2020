6b76bb180339be123ad9c7f3afdbced5b922b3a5
eldarofdeath <eldarofdeath@gmail.com>
2020-12-03 16:06:13 +0100

    Day 3 of AdventOfCode2020

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -10,8 +10,7 @@ import java.util.stream.Stream;
 public class app {
 
     public static void main(String[] args) {
-//        dayOne();
-        dayTwo();
+        dayThree();
     }
 
     private static void dayOne() {
@@ -92,5 +91,39 @@ public class app {
         }
 
 
+    }
+
+    private static void dayThree() {
+        String fileName = "src/main/resources/day3.txt";
+        List<char[]> map = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            map = stream.map(String::toCharArray).collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Day 4 part 1");
+        System.out.println(traverseTrees(map, 1, 3));
+        System.out.println("Day 4 part 2");
+        long result = traverseTrees(map, 1, 1) * traverseTrees(map, 1, 3) *
+                traverseTrees(map, 1, 5) * traverseTrees(map, 1, 7) * traverseTrees(map, 2, 1);
+        System.out.println(result);
+    }
+
+    private static long traverseTrees(List<char[]> map, int down, int right) {
+        int row = 0;
+        int column = 0;
+        long count = 0;
+        while (row < map.size() - 1) {
+            row += down;
+            column = (column + right) % map.get(row).length;
+            char ch = map.get(row)[column];
+            if (ch == '#') {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
     }
 }
